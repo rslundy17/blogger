784fb845b1edacdcb8cd2d79bb4c2be65e1387a9
RichardLundy <rslundy17@gmail.com>
2013-04-13 13:14:41 -0400

    Iteration 2 Done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,35 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
-end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(param[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(paramsp[:id])
+    @article.update_attributes(params[:article])
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
 end
